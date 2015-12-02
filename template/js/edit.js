1a8fa836620962d89a4324fb1226a051ca9f4f2d
Joseph Anz <janz@ucsd.edu>
2015-12-02 11:56:24 -0800

    included lines for dev and actual database

--- template/js/edit.js
+++ template/js/edit.js
@@ -1,5 +1,5 @@
 Parse.initialize("M0a7TBns2wo7HMdoULhac86LMnpjPothTzst4a1T", "cV4npfDqaSpeTLSwwyhYxg8CvoWqJc0QjXlM37c0");
-
+//Parse.initialize("3zsVEcWcoiBMqwA4kFftFSJk2kTIsCYY2Hc2dXJ0", "Gp6Mdb5ydKdPiiho32LOFzs5kcwMVqW3pVosxfJy");
 var urlParams;
 (window.onpopstate = function() {
   var match,
