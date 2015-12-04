6614b08dd64575d147a682ee1eaf27b6e2bef135
Joseph Anz <janz@ucsd.edu>
2015-12-04 12:57:14 -0800

    the destination of all the minified code

---
+++ template/destination/js/user.js
@@ -0,0 +1,1 @@
+Parse.initialize("M0a7TBns2wo7HMdoULhac86LMnpjPothTzst4a1T","cV4npfDqaSpeTLSwwyhYxg8CvoWqJc0QjXlM37c0");var currentUser=Parse.User.current();if(currentUser){console.log("user is logged in as:");var userinfo=JSON.stringify(currentUser);console.log(eval("("+userinfo+")"))}else window.location="../src/login.html";
